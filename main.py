48547cd413d5f7d01ff53b7ff9e8996f69469c05
shin-chan-agent <shinya.k.828@gmail.com>
2026-09-21 10:36:03 +0900

    main.py の更新

--- main.py
+++ main.py
@@ -344,7 +344,7 @@ def generate_and_send_line():
     prompt = get_article_prompt(
         theme,
         angle,
-        target_services,
+        services,
         knowledge,
         past_articles_text,
         current_date,
